7bf02161657d6ad415cc52154eb52e27190b607b
Damir555 <46638054+Damir555@users.noreply.github.com>
2026-07-03 22:55:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
+CORS_ORIGINS = [
+    "https://www.quiconvert.com",
+    "https://quiconvert.com"
+]
 
+MONETIZATION_ACTIVE = False
+
+DAILY_LIMIT_FREE = 5
+
+VALID_API_KEYS = {
+    "demo123",
+    "test456",
+    "admin789"
+}
+
+UPLOAD_FIELD = "files"
+
+MAX_UPLOAD_MB = 5
+MAX_UPLOAD_BYTES = MAX_UPLOAD_MB * 1024 * 1024
